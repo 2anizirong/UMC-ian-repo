4a793c28f23f743c9c5627912f57723b1cc7bcff
ian <hope030327@naver.com>
2024-11-04 01:28:17 +0900

    6주차 미션

--- UMC study/src/services/user.service.js
+++ UMC study/src/services/user.service.js
@@ -1,11 +1,13 @@
-import { responseFromUser } from '../dtos/user.dto.js';
-import {
-  addUser,
-  getUser,
-  getUserPreferencesByUserId,
-  setPreference,
-} from "../repositories/user.repository.js";
+import { getUserReview } from '../repositories/user.repository.js';
+import { responseFromReviews } from '../dtos/review.dto.js';
 
+// 사용자 리뷰 목록 조회
+export const listUserReview = async (userId, cursor) => {
+  const review = await getUserReview(userId, cursor);
+  return responseFromReview(review); // 변환된 리뷰 데이터 반환
+};
+
+// 사용자 가입 처리
 export const userSignUp = async (data) => {
   const joinUserId = await addUser({
     email: data.email,
@@ -21,12 +23,12 @@ export const userSignUp = async (data) => {
     throw new Error("이미 존재하는 이메일입니다.");
   }
 
-  for (const preference of data.preferences) {
+  for (const preference of data.preference) {
     await setPreference(joinUserId, preference);
   }
 
   const user = await getUser(joinUserId);
-  const preferences = await getUserPreferencesByUserId(joinUserId);
+  const preference = await getUserPreferencesByUserId(joinUserId);
 
-  return responseFromUser({ user, preferences });
+  return responseFromUser({ user, preference });
 };
